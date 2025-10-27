9627f8b8a76518ec2b2bbc6e9ff2fb60aa1d3268
izzypositivetech-001 <izzypositivetech@gmail.com>
2025-10-27 08:27:18 +0100

    server

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
 app.use(
   cors({
-    origin: function (origin, callback) {
-      const allowedOrigins = [
+    origin: [
         "http://localhost:5173", // local frontend
         "https://task-manager-client-virid.vercel.app/", // your deployed frontend
-      ];
-
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+      ],
     credentials: true, // allow cookies
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
